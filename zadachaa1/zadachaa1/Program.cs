710320fe058fe61a48161763286db7e0cbb7c5d7
osyaii <oscarcancer@yandex.ru>
2024-09-12 09:17:49 +0500

    первая задача

---
+++ zadachaa1/zadachaa1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadachaa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string result = Replacee();
+            Console.WriteLine(result);
+        }
+        static string Replacee()
+        {
+            string b = Console.ReadLine().Replace(".", "...");
+            return b;
+        }
+    }
+}
